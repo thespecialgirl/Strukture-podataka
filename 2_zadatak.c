8568e4f5bd3466cde4666192887d6d9222fbfb74
thespecialgirl <iv>
2025-10-22 22:56:38 +0200

    zad pod a i b

---
+++ 2_zadatak.c
@@ -0,0 +1,6 @@
+﻿Build started at 22:50...
+1>------ Build started: Project: 2zadatak, Configuration: Debug x64 ------
+1>2_zadatak.cpp
+1>2zadatak.vcxproj -> C:\Users\abc\source\repos\2zadatak\x64\Debug\2zadatak.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 22:50 and took 00,957 seconds ==========
